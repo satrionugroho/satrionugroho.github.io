aa8dfe88c0af2161abf6b311c4c4f31cc95d5736
Satrio Nugroho <satrio.nu27@gmail.com>
2017-08-24 16:50:29 +0700

    remove dependency to jQuery and add change color theme

--- index.md
+++ index.md
@@ -8,7 +8,7 @@ title: Home
     <li>
       {% assign date_format = site.minima.date_format | default: "%b %-d, %Y" %}
       <span class="post-meta">{{ post.date | date: date_format }}</span>
-      <h2>
+      <h2 class="throwable">
         <a class="post-link" href="{{ post.url | relative_url }}">{{ post.title | escape }}</a>
       </h2>
     </li>
---
+++ public/js/satrio.js
@@ -0,0 +1,49 @@
+const colors = [
+    "blue",
+    "green",
+    "purple",
+    "lime",
+    "black",
+    "red"
+  ];
+
+function changeTheme(position) {
+  var themeName = "theme-";
+  var selectedColors = colors[position];
+
+  return themeName + selectedColors;
+}
+
+function loadColorIfPresent() {
+  var savedColor = localStorage.getItem("satrioColorScheme");
+
+  if (savedColor !== null) {
+    document.body.className = changeTheme(savedColor);
+  } else {
+    initializeFirstColor();
+  }
+}
+
+function initializeFirstColor() {
+  if (typeof(Storage) !== undefined){
+    localStorage.setItem("satrioColorScheme", 0);
+  }
+}
+
+function updateColor(){
+  var counter = localStorage.getItem("satrioColorScheme");
+  console.log(counter);
+  var currentScheme = (counter == colors.length-1) ? 0 : parseInt(counter) + 1;
+
+  document.body.className = changeTheme(currentScheme);
+  localStorage.setItem("satrioColorScheme", currentScheme);
+}
+
+window.onload = loadColorIfPresent;
+
+document.addEventListener("DOMContentLoaded", function(){
+  var elem = document.getElementById("themeChange");
+  elem.addEventListener('click', function(){
+    updateColor();
+  });
+}, false);
